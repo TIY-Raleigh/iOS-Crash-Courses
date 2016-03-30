0c14ec587b93f6bb4e6e565be20e2b67f294c45a
David Rogers <hello@al-the-x.me>
2016-03-30 16:57:11 -0400

    Setup for FizzBuzz instead...

--- TailoredSwift.playground/Contents.swift
+++ TailoredSwift.playground/Contents.swift
@@ -1,5 +1,18 @@
-//: Playground - noun: a place where people can play
+//: **FizzBuzz** -- A Learning Game for Developers
+/*:
 
-import UIKit
+**GIVEN** the numbers from 1 to 100...
 
-var str = "Hello, playground"
+**WHEN** a number is evenly divisible by `3`
+**THEN** print the number followed by the word `"Fizz"`
+
+**WHEN** a number is evenly divisible by `5`
+**THEN** print the number followed by the word `"Buzz"`
+
+**WHEN** a number is evenly divisible by `3` _and_ `5`
+**THEN** print the number followed by the word `"FizzBuzz"`
+
+**WHEN** a number is divisible by _neither_ `3` _nor_ `5`
+**THEN** just print the number
+
+*/
